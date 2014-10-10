dbf32a9b0f2fb44da092139275b230b4315abf04
Fedor Tsarev <tsarevfs@gmail.com>
2014-10-10 20:51:17 +0400

    added simple Relax NG schema

--- README.txt
+++ README.txt
@@ -1,1 +1,4 @@
 This is a DSL for manage bioinformatics pipelines.
+
+Requires:
+jing for validating xml aganist RELAX NG schema https://code.google.com/p/jing-trang/
---
+++ Test/simple/check.sh
@@ -0,0 +1,3 @@
+PROJECT_ROOT=/home/fedor/DSL/BioinformaticsDSL/
+java -jar /home/fedor/DSL/tools/jing-20091111/bin/jing.jar -c $PROJECT_ROOT/Core/Syntax/lang.rng simple_good.xml
+java -jar /home/fedor/DSL/tools/jing-20091111/bin/jing.jar -c $PROJECT_ROOT/Core/Syntax/lang.rng simple_bad.xml
